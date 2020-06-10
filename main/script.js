08d51adf2e6fa00f8fba4cf02e8abcaca17a43ea
WuimmHoften <erickgarciavalenzuela@hotmail.com>
2020-06-09 20:42:51 -0600

    Primer avance

---
+++ main/script.js
@@ -0,0 +1,120 @@
+/* Please ❤ this if you like it! */
+
+
+(function($) { "use strict";
+		
+	//Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px", 
+		t.style.top = n.clientY + "px", 
+		e.style.left = n.clientX + "px", 
+		e.style.top = n.clientY + "px", 
+		i.style.left = n.clientX + "px", 
+		i.style.top = n.clientY + "px"
+    });
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+    function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+    }
+    function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+    }
+    s();
+    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+        o(r[a])
+    }
+    function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+    }
+	
+	//Navigation
+
+	var app = function () {
+		var body = undefined;
+		var menu = undefined;
+		var menuItems = undefined;
+		var init = function init() {
+			body = document.querySelector('body');
+			menu = document.querySelector('.menu-icon');
+			menuItems = document.querySelectorAll('.nav__list-item');
+			applyListeners();
+		};
+		var applyListeners = function applyListeners() {
+			menu.addEventListener('click', function () {
+				return toggleClass(body, 'nav-active');
+			});
+		};
+		var toggleClass = function toggleClass(element, stringClass) {
+			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+		};
+		init();
+	}();
+
+	
+	//Switch light/dark
+	
+	$("#switch").on('click', function () {
+		if ($("body").hasClass("light")) {
+			$("body").removeClass("light");
+			$("#switch").removeClass("switched");
+		}
+		else {
+			$("body").addClass("light");
+			$("#switch").addClass("switched");
+		}
+	});          
+              
+})(jQuery);
+
+const gallery = document.querySelector('.gallery');
+const overlay = document.querySelector('.overlay');
+const overlayImage = overlay.querySelector('img');
+const overlayClose = overlay.querySelector('.close');
+
+function generateHTML([h, v]) {
+  return `
+	<div class="item h${h} v${v}">
+	  <img src="../images/${randomNumber(11)}.jpg">
+	  <div class="item__overlay">
+		<button>View</button>
+	  </div>
+	</div>
+  `;
+}
+
+function randomNumber(limit) {
+  return Math.floor(Math.random() * limit) + 1;
+}
+
+function handleClick(e) {
+  const src = e.currentTarget.querySelector('img').src;
+  overlayImage.src = src;
+  overlay.classList.add('open');
+}
+
+
+function close() {
+  overlay.classList.remove('open');
+}
+
+const digits = Array.from({ length: 12 }, () => [randomNumber(4), randomNumber(4)]).concat([[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]])
+
+const html = digits.map(generateHTML).join('');
+gallery.innerHTML = html;
+
+const items = document.querySelectorAll('.item');
+
+items.forEach(item => item.addEventListener('click', handleClick));
+
+overlayClose.addEventListener('click', close);
+
+$( '.js-input' ).keyup(function() {
+	if( $(this).val() ) {
+	   $(this).addClass('not-empty');
+	} else {
+	   $(this).removeClass('not-empty');
+	}
+  });
